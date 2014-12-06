2fd0e4c1acede5067062166792fbaa9ad697290f
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2014-12-06 17:59:14 +0000

    fix(log): add log provider #27

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var app = angular.module('app', ["firebase", "ngSanitize"]);
+var app = angular.module("app", ["firebase", "ngSanitize"]);
 
-
-
+app.config(function($logProvider) {
+  $logProvider.debugEnabled(true);
+});
