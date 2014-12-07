b12837329d364e967a5b6472eb27c33bdbe122bd
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2014-12-07 14:18:07 +0000

    chore(js): cleanup

--- client/assets/js/shared/services/authentication_service.js
+++ client/assets/js/shared/services/authentication_service.js
@@ -2,7 +2,7 @@
 
 "use strict";
 
-app.factory('AuthenticationService', function($log, $firebaseAuth, $location, $rootScope, $localStorage, FIREBASE_URL){
+app.factory("AuthenticationService", function($log, $firebaseAuth, $location, $rootScope, $localStorage, FIREBASE_URL) {
   return function() {
     var that = this;
 
@@ -10,85 +10,87 @@ app.factory('AuthenticationService', function($log, $firebaseAuth, $location, $r
     that.fAuthData = fAuth.getAuth();
 
     that.checkIfUserExists = function(u_id) {
-      that.fAuth.child(u_id).once('value', function(snapshot) {
+      that.fAuth.child(u_id).once("value", function(snapshot) {
         return (snapshot.val() !== null);
       });
     };
 
-    that.authUsingFacebook = function(){
-      that.fAuth.authWithOAuthPopup("facebook", function(error, authData){
-          if(typeof authData === "undefined") return false;
-          return true;
+    that.authUsingFacebook = function() {
+      that.fAuth.authWithOAuthPopup("facebook", function(error, authData) {
+        if (typeof authData === "undefined") return false;
+        return true;
       });
     };
 
-    that.authUsingTwitter = function(){
-      that.fAuth.authWithOAuthPopup("twitter", function(error, authData){
-        if(typeof authData === "undefined") return false;
+    that.authUsingTwitter = function() {
+      that.fAuth.authWithOAuthPopup("twitter", function(error, authData) {
+        if (typeof authData === "undefined") return false;
         return true;
       });
     };
 
 
-    that.authUsingGithub = function(){
-      that.fAuth.authWithOAuthPopup("github", function(error, authData){
-          if(typeof authData === "undefined") return false;
-          return true;
+    that.authUsingGithub = function() {
+      that.fAuth.authWithOAuthPopup("github", function(error, authData) {
+        if (typeof authData === "undefined") return false;
+        return true;
       });
     };
 
 
-    that.authUsingGplus = function(){
-      that.fAuth.authWithOAuthPopup("google", function(error, authData){
-          if(typeof authData === "undefined") return false;
-          return true;
+    that.authUsingGplus = function() {
+      that.fAuth.authWithOAuthPopup("google", function(error, authData) {
+        if (typeof authData === "undefined") return false;
+        return true;
       });
     };
 
-    that.unauth = function(){
+    that.unauth = function() {
       that.fAuth.unauth();
-	  localStorage.removeItem('currentUser');
+      localStorage.removeItem("currentUser");
       $rootScope.currentUser = null;
-      window.location.href = '/';
+      window.location.href = "/";
     };
 
     that.fAuth.onAuth(function(authData) {
       var redirect = "messages";
-      if(typeof authData !== "undefined" && authData !== null){
+      if (typeof authData !== "undefined" && authData !== null) {
         var isNewUser = !that.checkIfUserExists(authData.uid);
-        if (authData && isNewUser) { that.fAuth.child(authData.uid).set(authData); }
+        if (authData && isNewUser) {
+          that.fAuth.child(authData.uid).set(authData);
+        }
         window.location.href = "/";
         that.fAuthData = authData;
         $rootScope.currentUser = that.getInfo();
       }
     });
-	
-    that.getInfo = function() {
-        var data = {};
-        data.uid = fAuthData.uid;
-        data.provider = fAuthData.provider;
-        data.lang = "en";
-        switch (data.provider) {
-          case "facebook":
-            data.name = fAuthData.facebook.displayName;
-            data.pic = "https://graph.facebook.com/" + fAuthData.facebook.cachedUserProfile.id + "/picture";
-            break;
-          case "twitter":
-            data.name = fAuthData.twitter.displayName;
-            data.pic = fAuthData.twitter.cachedUserProfile.profile_image_url;
-            break;
-          case "github":
-            data.name = fAuthData.github.displayName;
-            data.pic = fAuthData.github.cachedUserProfile.avatar_url;
-            break;
-          case "google":
-            data.name = fAuthData.google.displayName;
-            data.pic = fAuthData.google.cachedUserProfile.picture;
-            break;
-        }
 
-        localStorage.setItem('currentUser', JSON.stringify(data));
-        return data;
+    that.getInfo = function() {
+      var data = {};
+      data.uid = fAuthData.uid;
+      data.provider = fAuthData.provider;
+      data.lang = "en";
+      switch (data.provider) {
+        case "facebook":
+          data.name = fAuthData.facebook.displayName;
+          data.pic = "https://graph.facebook.com/" + fAuthData.facebook.cachedUserProfile.id + "/picture";
+          break;
+        case "twitter":
+          data.name = fAuthData.twitter.displayName;
+          data.pic = fAuthData.twitter.cachedUserProfile.profile_image_url;
+          break;
+        case "github":
+          data.name = fAuthData.github.displayName;
+          data.pic = fAuthData.github.cachedUserProfile.avatar_url;
+          break;
+        case "google":
+          data.name = fAuthData.google.displayName;
+          data.pic = fAuthData.google.cachedUserProfile.picture;
+          break;
+      }
+
+      localStorage.setItem("currentUser", JSON.stringify(data));
+      return data;
     };
 
     return that;
