e14fe67d4a78893fa83ca88959720417e669a625
Kleber Correia <klebervirgilio@gmail.com>
2014-12-06 23:31:31 -0200

    chore(remove-code) remove console.log .

--- client/assets/js/shared/controllers/header_controller.js
+++ client/assets/js/shared/controllers/header_controller.js
@@ -1,5 +1,5 @@
 app.controller('HeaderCtrl', function($scope, AuthenticationService){
   $scope.unauth = function(){
-    console.log(AuthenticationService().unauth());
+    AuthenticationService().unauth();
   };
 });
