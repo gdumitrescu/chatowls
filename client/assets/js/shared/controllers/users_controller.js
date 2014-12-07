59b8239ab7d7b25f45b7d77983f89b34f3e47ca8
Rajesh Veeranki <rveeranki01@gmail.com>
2014-12-07 15:57:50 +0530

    Modified display users

--- client/assets/js/shared/controllers/users_controller.js
+++ client/assets/js/shared/controllers/users_controller.js
@@ -13,28 +13,28 @@ app.controller("UsersController", function($scope, $firebase) {
 	ref.orderByChild("expires").on("child_added", function(snapshot) {
 	  if(snapshot.val().provider == "facebook" && snapshot.val().facebook.cachedUserProfile.locale != undefined){
 	  	//console.log(snapshot.val().facebook.cachedUserProfile.first_name + " uses " + snapshot.val().facebook.cachedUserProfile.locale + " language");
-	  	var user = { 'name': snapshot.val().facebook.cachedUserProfile.first_name , 'lang' : snapshot.val().facebook.cachedUserProfile.locale, 'provider' : 'fb'};
+	  	var user = { 'name': snapshot.val().facebook.cachedUserProfile.first_name , 'lang' : snapshot.val().facebook.cachedUserProfile.locale, 'provider' : 'facebook'};
 	  	$scope.$apply(function(){
 	  		$scope.users.push(user);
 	  	});
 	  }
 	  else if (snapshot.val().provider == "github" && snapshot.val().github.cachedUserProfile.location != undefined){
 	  	//console.log(snapshot.val().twitter.cachedUserProfile.login + " is in " + snapshot.val().github.cachedUserProfile.location + " " );
-	  	var user = { 'name': snapshot.val().twitter.cachedUserProfile.login , 'lang' : snapshot.val().github.cachedUserProfile.location, 'provider':'gh'};
+	  	var user = { 'name': snapshot.val().twitter.cachedUserProfile.login , 'lang' : snapshot.val().github.cachedUserProfile.location, 'provider':'github'};
 	  	$scope.$apply(function(){
 	  		$scope.users.push(user);
 	  	});
 	  }
 	  else if (snapshot.val().provider == "twitter" && snapshot.val().twitter.cachedUserProfile.lang != undefined){
 	  	//console.log(snapshot.val().twitter.cachedUserProfile.name + " uses " +  snapshot.val().twitter.cachedUserProfile.lang + " "  );
-	  	var user = {'name': snapshot.val().twitter.cachedUserProfile.name , 'lang' : snapshot.val().twitter.cachedUserProfile.lang, 'provider':'tw'};
+	  	var user = {'name': snapshot.val().twitter.cachedUserProfile.name , 'lang' : snapshot.val().twitter.cachedUserProfile.lang, 'provider':'twitter'};
 	  	$scope.$apply(function(){
 	  		$scope.users.push(user);
 	   });
 	  }
 	  else if (snapshot.val().provider == "google" && snapshot.val().google.cachedUserProfile.locale != undefined){
 	  	//console.log(snapshot.val().google.cachedUserProfile.given_name+ " uses " + snapshot.val().google.cachedUserProfile.locale + " language");
-	  	var user = {'name':snapshot.val().google.cachedUserProfile.given_name, 'lang':snapshot.val().google.cachedUserProfile.locale, 'provider':'gl' };
+	  	var user = {'name':snapshot.val().google.cachedUserProfile.given_name, 'lang':snapshot.val().google.cachedUserProfile.locale, 'provider':'google' };
 	  	$scope.$apply(function(){
 	  		$scope.users.push(user);
 	    });
